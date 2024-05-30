d41d9a06a5af5acb9252195077930d6aade8ba21
shamshir-hussain <shamshir.hussain08@gmail.com>
2024-05-30 21:34:28 +0530

    added new lines

--- arrays_basicconcepts/practice.js
+++ arrays_basicconcepts/practice.js
@@ -169,3 +169,30 @@
 //   }
 // }
 // console.log(largest);
+
+//QS:Create a program that generates a random number representing a dice roll.[The number should be between 1 and 6].
+
+// let dice = Math.floor(Math.random() * 6 + 1);
+// console.log(dice);
+
+//QS:Create an object representing a car that stores the following properties for the car: name, model, color. Print the car’s name.
+
+// const cars = {
+//   name: "BMW",
+//   model: "X-Series",
+//   color: "Black",
+// };
+
+// console.log(cars.name); //BMW
+
+//QS:Create an object Person with their name, age and city. Edit their city’s original value to change it to “New York”.Add a new property country and set it to the United States.
+
+// const person = {
+//   name: "Shamshir",
+//   age: 23,
+//   city: "Nandyal",
+// };
+
+// person.city = "New York";
+// person.country = "United States";
+// console.log(person);
--- objects/script.js
+++ objects/script.js
@@ -54,3 +54,121 @@
 // };
 
 // console.log(obj.true, obj[1], obj.2); //c, a, unexpected error
+
+//add/update/delete:
+
+// const student = {
+//   name: "shamshir",
+//   age: 23,
+//   marks: 92,
+//   city: "Nandyal",
+// };
+
+// console.log((student.marks = "A")); //updated to A
+// console.log((student.city = "Hyderabad")); // updated to hyderabad
+// console.log((student.gender = "Male")); //added gender
+// console.log(student); //{name: 'shamshir', age: 23, marks: 'A', city: 'Hyderabad', gender: 'Male'}
+// console.log(delete student.gender); //it returns "true" if deleted.
+// console.log(student); //{name: 'shamshir', age: 23, marks: 'A', city: 'Hyderabad'}
+
+//nested objects
+
+// const classInfo = {
+//   shamshir: {
+//     grade: "A+",
+//     city: "Nandyal",
+//   },
+//   munawar: {
+//     grade: "A",
+//     city: "Kurnool",
+//   },
+//   samatha: {
+//     grade: "O",
+//     city: "Bhadrachalam",
+//   },
+// };
+// console.log(classInfo); //{shamshir: {…}, munawar: {…}, samatha: {…}}
+// console.log(classInfo.shamshir); //{grade: 'A+', city: 'Nandyal'}
+// console.log(classInfo.shamshir.grade); //A+
+
+//Array of objects
+
+// const classInfo = [
+//   {
+//     name: "shamshir",
+//     grade: "A+",
+//     city: "Nandyal",
+//   },
+//   {
+//     name: "samatha",
+//     grade: "O",
+//     city: "Bhadrachalam",
+//   },
+//   {
+//     name: "Munawar",
+//     grade: "A",
+//     city: "Kurnool",
+//   },
+// ];
+// console.log(classInfo); //(3) [{…}, {…}, {…}]
+// console.log(classInfo[0]); //{name: 'shamshir', grade: 'A+', city: 'Nandyal'}
+// console.log(classInfo[1]); //{name: 'samatha', grade: 'O', city: 'Bhadrachalam'}
+// console.log(classInfo[0].city); //Nandyal
+// console.log((classInfo[0].city = "Hyderabad")); //Hyderabad
+
+//Random Integers from 1 to 10
+
+// let step1 = Math.random();
+// console.log(step1); // generates random => 0.3737437891402273
+// let step2 = step1 * 10;
+// console.log(step2); //generates 0 to 10 => 3.737437891402273
+// let step3 = step2 + 1;
+// console.log(step3); // generates 1 to 10 => 4.737437891402273
+// let step4 = Math.floor(step3);
+// console.log(step4); // round to nearest and smallest value => 4
+
+// let random = Math.floor(Math.random() * 10 + 1);
+// console.log(random);
+
+//practice QS:
+
+//QS: generate a random number from 1 and 100
+
+// let random = Math.floor(Math.random() * 100 + 1);
+
+// console.log(random);
+
+// generate from 21 to 25
+// let random = Math.floor(Math.random() * (25 - 21 + 1)) + 21;
+// console.log(random);
+//or
+// let random = Math.floor(Math.random() * 5 + 21);
+// console.log(random);
+
+// Guessing game using random numbers:
+
+// const max = prompt("Enter your max number");
+
+// const random = Math.floor(Math.random() * max + 1);
+
+// let guess = prompt("Guess the number");
+
+// while (true) {
+//   if (guess == "quit") {
+//     console.log("You quit the game");
+//     break;
+//   }
+
+//   if (guess == random) {
+//     console.log(`Your guess was right! Congrats!! random number ${random}`);
+//     break;
+//   } else if (guess < random) {
+//     guess = prompt(
+//       `hint :Your guess was too small... Guessed number ${guess}!! Try again`
+//     );
+//   } else {
+//     guess = prompt(
+//       `hint :Your guess was too large... Guessed number ${guess}!! Try again`
+//     );
+//   }
+// }
