af5fa9958ad303bf8b8aeab2b06e40dde1e2f178
shamshir-hussain <shamshir.hussain08@gmail.com>
2024-05-31 20:08:21 +0530

    added

--- arrays_basicconcepts/practice.js
+++ arrays_basicconcepts/practice.js
@@ -196,3 +196,69 @@
 // person.city = "New York";
 // person.country = "United States";
 // console.log(person);
+
+// functions
+
+//create a function that prints a poem
+
+// function poem() {
+//   console.log("Twinkle Twinkle little star");
+//   console.log("How i wonder what you are ");
+//   console.log("Up above the world so high,");
+//   console.log("Like a diamond in the sky.");
+//   console.log("When the blazing sun is gone,");
+//   console.log("When he nothing shines upon,");
+//   console.log("Then you show your little light,");
+//   console.log("Twinkle, twinkle, all the night.");
+//   console.log("Then the traveller in the dark,");
+//   console.log("Thanks you for your tiny spark,");
+//   console.log("He could not see which way to go,");
+//   console.log("If you did not twinkle so.");
+//   console.log("In the dark blue sky you keep,");
+//   console.log("And often through my curtains peep,");
+//   console.log("For you never shut your eye,");
+//   console.log("‘Till the sun is in the sky.");
+//   console.log("As your bright and tiny spark,");
+//   console.log("Lights the traveller in the dark");
+//   console.log("Though I know not what you are,");
+//   console.log("Twinkle, twinkle, little star.");
+// }
+
+// poem();
+
+//create a function to roll a dice & always display the value of the the dice(1 to 6)
+
+// function rollDice() {
+//     let rand = Math.floor(Math.random() * 6 + 1);
+//     console.log(rand);
+//   }
+
+//   rollDice();
+
+//create a function that gives us the average of 3 numbers
+
+// function calcAvg(a, b, c) {
+//     let avg = (a + b + c) / 3;
+//     console.log(avg);
+//   }
+  
+//   calcAvg(2, 4, 6); //4
+
+//create a function that prints the multiplication table of a number
+
+// function number(num) {
+//     for (let i = 1; i <= 10; i++) {
+//       let mul = num * i;
+//       console.log(`${num}*${i}=${mul}`);
+//     }
+//   }
+  
+//   number(2);
+
+// function printTable(n) {
+//     for (let i = n; i <= n * 10; i += n) {
+//       console.log(i);
+//     }
+//   }
+  
+//   printTable(13);
